03bf6be633b3b476a5b089ef89f44029d993f427
Wasif Khan <waasif.khaan@gmail.com>
2018-09-04 12:51:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,12 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 	  user = User.find_by(:username =>params[:username])
+	  if user != nil
+	    redirect "/success"
+	  else 
+	    redirect to "/failure"
+	  end 
+	    
 		#your code here!
 	end
 
